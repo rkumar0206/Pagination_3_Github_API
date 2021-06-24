2ed75f0f356c324bd17d116d3f7259dbb72bfd26
Rohit <rkumar8092378845@gmail.com>
2021-06-24 12:27:36 +0530

    -> Added hilt dependency

--- app/src/main/java/com/rohitthebest/pagination_3_github_api/SearchRepositoriesActivity.kt
+++ app/src/main/java/com/rohitthebest/pagination_3_github_api/SearchRepositoriesActivity.kt
@@ -3,9 +3,9 @@ package com.rohitthebest.pagination_3_github_api
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SearchRepositoriesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search_repositories)
     }
 }
---
+++ app/src/main/java/com/rohitthebest/pagination_3_github_api/SearchRepositoryApplication.kt
@@ -0,0 +1,8 @@
+package com.rohitthebest.pagination_3_github_api
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SearchRepositoryApplication : Application() {
+}
