a70346c2894c7e5db1d1e93cfa70ce801e0852b6
Rohit <rkumar8092378845@gmail.com>
2021-06-26 00:43:09 +0530

    -> overridden the initialize function in GithubRemoteMediator.kt

--- app/src/main/java/com/rohitthebest/pagination_3_github_api/data/GithubRemoteMediator.kt
+++ app/src/main/java/com/rohitthebest/pagination_3_github_api/data/GithubRemoteMediator.kt
@@ -24,6 +24,16 @@ class GithubRemoteMediator(
     private val repoDatabase: RepoDatabase
 ) : RemoteMediator<Int, Repo>() {
 
+    override suspend fun initialize(): InitializeAction {
+
+        // Launch remote refresh as soon as paging starts and do not trigger remote prepend or
+        // append until refresh has succeeded. In cases where we don't mind showing out-of-date,
+        // cached offline data, we can return SKIP_INITIAL_REFRESH instead to prevent paging
+        // triggering remote refresh.
+
+        return InitializeAction.LAUNCH_INITIAL_REFRESH
+    }
+
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Repo>): MediatorResult {
 
         val page = when (loadType) {
